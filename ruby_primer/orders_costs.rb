126b3a782444f799c32aabe23cad5cbd57de38de
raluka <badoi.raluca@gmail.com>
2014-06-26 18:58:01 +0200

    Orders and costs

---
+++ ruby_primer/orders_costs.rb
@@ -0,0 +1,26 @@
+=begin
+Orders and costs
+
+Problem Statement
+A restaurant has incoming orders for which you need to compute the costs based on the menu.
+
+You might get multiple orders at times.
+
+Examples:
+if the order is {:rice => 1, :noodles => 1}, and the menu is {:rice => 3, :noodles => 2}, then the cost is 5.
+
+If the orders are {:rice => 1, :noodles => 1} and {:rice => 2, :noodles => 2} and the menu is
+{:rice => 3, :noodles => 2}, then the cost is 15.
+=end
+
+class Restaurant
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def cost(*orders)
+    orders.inject(0) do |total_price, order|
+      total_price + order.keys.inject(0) {|cost, item| cost + @menu[item]*order[item]}
+    end
+  end
+end
